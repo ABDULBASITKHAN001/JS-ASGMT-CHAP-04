8ef8c6c933f5b99a6a4fafb9e8a998f05afd02fd
ABDULBASITKHAN001 <[basitkhurramkhan@gmail.com]>
2025-02-05 16:39:18 +0500

    JS-Chap-04

---
+++ script.js
@@ -0,0 +1,20 @@
+  // =======================================CHAPTER 4  LEGAL & ILLEGAL=================================
+               // QUESTION NO.1: Declare 3 variables in one statement.
+               var x = 10, y = 20, z = 30;
+               alert(x + " " + y + " " + z)
+
+
+              // QUESTION NO.2: Declare 5 legal & 5 illegal variable names
+
+              // legal variables
+               var _myVariable = 10;
+               var $myVariable = 20;
+               var myVariable = 30;
+               var my_variable = 40;
+               
+                 //ILLEGAL VARIABLE NAME
+                //var 1myVariable = 50;
+               //var myVariable123 = 60;
+              //var myVariable$ = 70;
+             //var myVariable@ = 80;
+            //var myVariable = 90; 
